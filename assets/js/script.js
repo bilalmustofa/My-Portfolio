2b90e870ea28f5cd1e33db5b212909c17e299c5c
bilal <bilalmustofa146@gmail.com>
2026-05-06 12:32:29 -0600

    add typing letter animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 window.addEventListener('load', () => {
     document.querySelector('.header').classList.add('show');
   });
+  // Letter Typing Animation
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
